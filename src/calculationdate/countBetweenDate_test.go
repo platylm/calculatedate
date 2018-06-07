1ccb091c227917e658863775daeab2c33e064c0a
gunner1886 <johndoe@example.com>
2018-06-07 13:46:33 +0700

    [Add]: create test for transferdatetosecond()

--- src/calculationdate/countBetweenDate_test.go
+++ src/calculationdate/countBetweenDate_test.go
@@ -43,3 +43,41 @@ func Test_Cal_Between_Date_Input_Startday_Endday_Should_Be_9079(t *testing.T) {
 	}
 
 }
+
+func Test_Tranfer_Day_To_Minute_Input_152_Should_Be_218880(t *testing.T) {
+
+	day := 152
+	resultMinute := TranferDayToMinute(day)
+	expectedMinute := "218880"
+
+	if resultMinute != expectedMinute {
+		t.Errorf("expectedMinute %s but %s ", expectedMinute, resultMinute)
+
+	}
+
+}
+
+func Test_Tranfer_Day_To_Minute_Input_152_Should_Be_13073760(t *testing.T) {
+
+	day := 9079
+	resultMinute := TranferDayToMinute(day)
+	expectedMinute := "13073760"
+
+	if resultMinute != expectedMinute {
+		t.Errorf("expectedHour %s but %s ", expectedMinute, resultMinute)
+
+	}
+
+}
+
+func Test_Tranfer_Day_To_Second_Input_152_Should_Be_13132800(t *testing.T) {
+	day := 152
+	resultHour := TranferDayToSecond(day)
+	expectedHour := "13132800"
+
+	if resultHour != expectedHour {
+		t.Errorf("expectedSecond %s but %s ", expectedHour, resultHour)
+
+	}
+
+}
