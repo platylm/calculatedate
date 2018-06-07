8ebb35cb954904d7377061f5d3ff2f791b18b48d
CAPTAINAMERICA <captainamerica@CAPTAINAMERICAs-Mac-mini.local>
2018-06-07 15:44:24 +0700

    [Edit]: function MakeJSON

--- src/calculationdate/calculationdate.go
+++ src/calculationdate/calculationdate.go
@@ -12,6 +12,32 @@ const year = 365
 const second = 60
 const week = 7
 
+type durationDate struct {
+	DayNameFrom   string `json:"daynamefrom"`
+	DayNameTo     string `json:"daynameto"`
+	ResultDay     string `json:"resultday"`
+	Second        string `json:"second"`
+	Minute        string `json:"minute"`
+	Hours         string `json:"hours"`
+	Week          string `json:"week"`
+	DayOverOfWeek string `json:"dayoverofweek"`
+	Percent       string `json:"percent"`
+}
+
+func MakeJSON(startday int, startmonth int, startyear int, endday int, endmonth int, endyear int) durationDate {
+	day, _ := strconv.Atoi(CalBetweenDate(startday, startmonth, startyear, endday, endmonth, endyear))
+	return durationDate{
+		DayNameFrom: SetFullNameDate(startday, startmonth, startyear),
+		DayNameTo:   SetFullNameDate(endday, endmonth, endyear),
+		ResultDay:   CalBetweenDate(startday, startmonth, startyear, endday, endmonth, endyear),
+		Second:      TransferDayToSecond(day),
+		Minute:      TransferDayToMinute(day),
+		Hours:       TransferDayToHours(day),
+		// Week:        TransferDayToWeek(day),
+		Percent: CalculatePercentOfYears(day),
+	}
+}
+
 func CalBetweenDate(startday int, startmonth int, startyear int, endday int, endmonth int, endyear int) string {
 
 	startdate := time.Date(startyear, time.Month(startmonth), startday, 0, 0, 0, 0, time.UTC)
@@ -26,46 +52,36 @@ func CalBetweenDate(startday int, startmonth int, startyear int, endday int, end
 }
 
 func TransferDayToMinute(days int) string {
-
 	minutes := days * hour * minute
-
 	return strconv.Itoa(minutes)
 
 }
 
 func TransferDayToSecond(days int) string {
-
 	seconds := days * hour * minute * second
 	return strconv.Itoa(seconds)
 }
 
 func TransferDayToWeek(days int) (string, string) {
-
 	weeks := days / week
 	overdays := days % week
-
 	return strconv.Itoa(weeks), strconv.Itoa(overdays)
 
 }
 func TransferDayToHours(days int) string {
-
 	hours := days * hour
 	return strconv.Itoa(hours)
 }
 
 func SetFullNameDate(year int, month int, day int) string {
-
 	fullnamedate := time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.UTC)
-
 	weekdays := fullnamedate.Weekday()
 	days := fullnamedate.Day()
 	months := fullnamedate.Month()
 	years := fullnamedate.Year()
-
 	return fmt.Sprintf("%s %s %s %s", weekdays.String(), strconv.Itoa(days), months.String(), strconv.Itoa(years))
 }
 func CalculatePercentOfYears(days int) string {
-
 	percentofyears := (float64(days) / year) * 100
 	return strconv.FormatFloat(percentofyears, 'f', 2, 64)
 }
@@ -74,6 +90,5 @@ func CheckEqualDay(startday, startmouth, startyear, endday, endmonth, endyear in
 	if startday == endday && startmouth == endmonth && startyear == endyear {
 		return "1"
 	}
-
 	return "not equal"
 }
