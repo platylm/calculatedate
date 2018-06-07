611a7679ee2c07e29e6382c65c0e37426262aab9
gunner1886 <johndoe@example.com>
2018-06-07 12:59:57 +0700

    [Create]:add function CalBetweenDate

--- src/calculationdate/calculationdate.go
+++ src/calculationdate/calculationdate.go
@@ -1,6 +1,23 @@
 package calculationdate
 
-func Cal() string {
-	return "ploy"
+import (
+	"strconv"
+	//"calculationdate"
+
+	"time"
+)
+
+const hour = 24
+
+func CalBetweenDate(startday int, startmonth int, startyear int, endday int, endmonth int, endyear int) string {
+
+	startdate := time.Date(startyear, time.Month(startmonth), startday, 0, 0, 0, 0, time.UTC)
+	enddate := time.Date(endyear, time.Month(endmonth), endday, 0, 0, 0, 0, time.UTC)
+
+	diff := enddate.Sub(startdate)
+
+	hours := diff.Hours()
+	days := (hours / hour) + 1
+	return strconv.FormatFloat(days, 'f', 0, 64)
 
 }
